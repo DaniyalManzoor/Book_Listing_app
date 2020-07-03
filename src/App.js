005c74e5cc26fadbde490106f53f71beeaa273db
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-07-03 15:49:25 +0500

    create Bookcontext.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import BookContextProvider from "./contexts/bookContext";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BookContextProvider></BookContextProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/contexts/bookContext.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useState } from "react";
+export const BookContext = createContext();
+
+const BookContextProvider = ({ children }) => {
+  const [books, setBooks] = useState([
+    { id: 1, title: "name of the wind", author: "patrick rothfuss" },
+    { id: 2, title: "the final empire", author: "brandon sanderson" },
+  ]);
+
+  const addBooks = (title, author) => {
+    setBooks([
+      ...books,
+      { id: Math.floor(Math.random() * 100000000), title, author },
+    ]);
+  };
+
+  const removeBook = (id) => {
+    setBooks([books.filter((book) => book.id !== id)]);
+  };
+
+  return (
+    <BookContext.Provider value={{ books, addBooks, removeBook }}>
+      {children}
+    </BookContext.Provider>
+  );
+};
+
+export default BookContextProvider;
