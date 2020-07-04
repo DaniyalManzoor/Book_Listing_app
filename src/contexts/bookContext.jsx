54d4bffbd68a00e023c5250075343757ad6398ec
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-07-04 22:06:46 +0500

    adding local Stroage

--- src/contexts/bookContext.jsx
+++ src/contexts/bookContext.jsx
@@ -1,13 +1,17 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { BookReducer } from "./../reducers/bookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = ({ children }) => {
-  const [books, dispatch] = useReducer(BookReducer, [
-    { id: 1, title: "name of the wind", author: "patrick rothfuss" },
-    { id: 2, title: "the final empire", author: "brandon sanderson" }
-  ]);
+  const [books, dispatch] = useReducer(BookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return (
     <BookContext.Provider value={{ books, dispatch }}>
